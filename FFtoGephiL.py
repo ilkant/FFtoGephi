c9e62934cfa76087c585e9ae5434ce2073fa8e77
Ilpo Kantonen <ilkant@users.noreply.github.com>
2020-08-22 22:13:29 +0300

    Create FFtoGephiL.py

---
+++ FFtoGephiL.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+from csv import reader
+
+with open('FTDNA_FF_FILE.csv', 'r') as read_obj:
+    csv_reader = reader(read_obj)
+    i = 0
+    s = "Undirected"
+    
+    print("Source,Target,Type,Id,Label,timeset,Weight")
+
+    for row in csv_reader:
+        i += 1
+        if i > 1:
+            print('%d,%d,Undirected,%d,,,%d' %(0,i-1,i-2,int(float(row[7]))))
